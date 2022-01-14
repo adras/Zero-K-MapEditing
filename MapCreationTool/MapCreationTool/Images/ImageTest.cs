34f96e2ea0b189caa94faaf90a23771dfe344775
Andreas Kurka <andreas.kurka@adesso.de>
2022-01-14 13:55:51 +0100

    Images are now created with an arbitrary color Started implementation of settings for map compiler

--- MapCreationTool/MapCreationTool/Images/ImageTest.cs
+++ MapCreationTool/MapCreationTool/Images/ImageTest.cs
@@ -1,7 +1,10 @@
 ﻿using MapCreationTool.Models;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Bmp;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,10 +14,10 @@ using System.Threading.Tasks;
 
 namespace MapCreationTool.Images
 {
-    // Library overview: https://csharp-station.com/4-best-c-libraries-for-image-processing/
-    // More libraries: https://stackoverflow.com/questions/5881031/image-processing-libraries-in-c-sharp
-    // Maybe better than ImageProcessor: https://github.com/SixLabors/ImageSharp;
-    /*
+	// Library overview: https://csharp-station.com/4-best-c-libraries-for-image-processing/
+	// More libraries: https://stackoverflow.com/questions/5881031/image-processing-libraries-in-c-sharp
+	// Maybe better than ImageProcessor: https://github.com/SixLabors/ImageSharp;
+	/*
     Image formats
     Diffuse - multiple of 1024 - no alpha, bmp
     Height - 16 bit grayscale, bmp?
@@ -24,23 +27,37 @@ namespace MapCreationTool.Images
     typemap- bmp? 
     minimap - 1024x1024
     */
-    class ImageTest
-    {
-        public static void CreateImages(MapSizeDefinition mapSize, string mapDir)
-        {
-            //SixLabors.ImageSharp.PixelFormats.Rgb24
-            Image<Rgb24> diffuse = new Image<Rgb24>(mapSize.diffuseMapSize.width, mapSize.diffuseMapSize.height);
-            Image<Rgb24> grass = new Image<Rgb24>(mapSize.grassMapSize.width, mapSize.grassMapSize.height);
-            Image<Rgb24> height = new Image<Rgb24>(mapSize.heightMapSize.width, mapSize.heightMapSize.height);
-            Image<Rgb24> metal = new Image<Rgb24>(mapSize.metalMapSize.width, mapSize.metalMapSize.height);
+	class ImageTest
+	{
 
+		public static void CreateImages(MapSizeDefinition mapSize, string mapDir)
+		{
+			//SixLabors.ImageSharp.PixelFormats.Rgb24
+			Image<Rgb24> diffuse = new Image<Rgb24>(mapSize.diffuseMapSize.width, mapSize.diffuseMapSize.height);
+			Image<Rgb24> grass = new Image<Rgb24>(mapSize.grassMapSize.width, mapSize.grassMapSize.height);
+			Image<Rgb24> height = new Image<Rgb24>(mapSize.heightMapSize.width, mapSize.heightMapSize.height);
+			Image<Rgb24> metal = new Image<Rgb24>(mapSize.metalMapSize.width, mapSize.metalMapSize.height);
 
-            BmpEncoder encoder = new BmpEncoder();
+			// Fill images with an arbitrary background color, otherwise images would be empty files
+			diffuse.Mutate(x => x.Fill(Color.Aqua));
+			grass.Mutate(x => x.Fill(Color.Purple));
+			height.Mutate(x => x.Fill(Color.PeachPuff));
+			metal.Mutate(x => x.Fill(Color.Sienna));
 
-            encoder.Encode(diffuse, File.Create($"{mapDir}\\diffuse.bmp"));
-            encoder.Encode(grass, File.Create($"{mapDir}\\grass.bmp"));
-            encoder.Encode(height, File.Create($"{mapDir}\\height.bmp"));
-            encoder.Encode(metal, File.Create($"{mapDir}\\metal.bmp"));
-        }
-    }
+			BmpEncoder encoder = new BmpEncoder();
+
+			WriteImage(encoder, diffuse, $"{mapDir}\\diffuse.bmp");
+			WriteImage(encoder, grass, $"{mapDir}\\grass.bmp");
+			WriteImage(encoder, height,$"{mapDir}\\height.bmp");
+			WriteImage(encoder, metal, $"{mapDir}\\metal.bmp");
+		}
+
+		private static void WriteImage<T>(IImageEncoder encoder, Image<T> image, string fileName) where T: unmanaged, IPixel<T>
+		{
+			using (Stream stream = File.Create(fileName))
+			{
+				encoder.Encode<T>(image, stream);
+			}
+		}
+	}
 }
---
+++ MapCreationTool/MapCreationTool/MapConverter/MapCompiler.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapCreationTool.MapConverter
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	class MapCompilerSetting
+	{
+		public readonly string identifier;
+		public readonly string settingValue;
+		public readonly string custom;
+
+		// Use this overload for custom settings which are not composed out of identifier, and setting value
+		public MapCompilerSetting(string custom)
+		{
+			this.custom = custom;
+		}
+
+		public MapCompilerSetting(string identifier, string settingValue)
+		{
+			this.identifier = identifier;
+			this.settingValue = settingValue;
+		}
+
+		public override string ToString()
+		{
+			string result;
+			if (string.IsNullOrEmpty(custom))
+			{
+				result = $"{identifier} {settingValue}";
+			}
+			else
+			{
+				result = $"{custom}";
+			}
+			return result;
+		}
+	}
+
+	class MapCompilerSettings
+	{
+		List<MapCompilerSetting> settings;
+		public MapCompilerSettings()
+		{
+			settings = new List<MapCompilerSetting>();
+		}
+
+		public void AddSetting(MapCompilerSetting setting)
+		{
+			settings.Add(setting);
+		}
+
+		public void Clear()
+		{
+			settings.Clear();
+		}
+
+		public bool Remove(MapCompilerSetting setting)
+		{
+			bool result = settings.Remove(setting);
+			return result;
+		}
+
+		public bool Remove(string identifier)
+		{
+			MapCompilerSetting? setting = settings.FirstOrDefault(s => s.identifier == identifier);
+			if (setting == null)
+				return false;
+
+			bool result = settings.Remove(setting);
+			return result;
+		}
+
+		public string GenerateParameterString()
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (MapCompilerSetting setting in settings)
+			{
+				// Using ToString overload
+				sb.Append(setting);
+			}
+			string result = sb.ToString();
+			return result;
+		}
+	}
+
+	class MapCompiler
+	{
+		public static void Compile(List<MapCompilerSetting> settings)
+		{
+			// Spring Map Format (SMF) compiler parameters
+			// Not all parameter descriptions added, see springrts_smf_compiler\pymapconf.py for descriptions
+			// OutFile -o, --outfile <output mapname.smf> (required) The name of the created map file. Should end in .smf. A tilefile (extension .smt) is also created, this name may contain spaces',
+			// DiffuseMap -t --intex <texturemap.bmp> (required) Input bitmap to use for the map. Sizes must be multiples of 1024. Xsize and Ysize are determined from this file; xsize = intex width / 8, ysize = height / 8. Don\'t use Alpha unless you know what you are doing.',
+			// HeightMap -a --heightmap help='|HEIGHT MAP| <heightmap file> (required) Input heightmap to use for the map, this should be 16 bit greyscale PNG image or a 16bit intel byte order single channel .raw image. Must be xsize*64+1 by ysize*64+1',
+			// MetalMap -m --metalmap help='|METAL MAP| <metalmap.bmp> Metal map to use, red channel is amount of metal. Resized to xsize / 2 by ysize / 2.',
+			// MaxHeight -x --maxheight
+			// MinHeight -n --minheight
+			// GeoventDecal -g --geoventfile
+			// Compression -c --compress
+			// Invert -i --invert
+			// Lowpass -l --lowpass
+			// Featureplacement -k --featureplacement
+			// Featurelist -j --featurelist
+			// FeatureMap -f --featuremap
+			// GrassMap -r --grassmap
+			// TypeMap -y --typemap
+			// MiniMap -p --minimap
+			// JustSmf -s --justsmf
+			// nvxdoptions -v --nvdxt_options
+			// HighresFilter --highresheightmapfilter
+			// Dirty -c --dirty
+			// Decompile -d --decompile
+			// SkipTexture -s --skiptexture
+
+			// Mandatory: --outfile, --intex, --heightmap, --metalmap, --maxheight, --minheight
+		}
+	}
+}
