63d2ad2d4768d9e42d576c05a3b2cacb8fa45a21
Andreas Kurka <andreas.kurka@gmail.com>
2022-01-15 15:37:22 +0100

    Added stub for PathHelper

---
+++ MapCreationTool/MapCreationTool/Helpers/PathHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapCreationTool.Helpers
+{
+    public class PathHelper
+    {
+        // TODO: Add methods which help to create the paths required to create a map
+        // blueprint path, map folder, etc
+    }
+}
