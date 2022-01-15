a77388234f177a981d6be6fdc8b64888b63e1930
Andreas Kurka <andreas.kurka@gmail.com>
2022-01-15 16:45:55 +0100

    Changed window from NavigationWindow to Window

--- MapCreationTool/MapCreationTool/MainWindow.xaml.cs
+++ MapCreationTool/MapCreationTool/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace MapCreationTool
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
