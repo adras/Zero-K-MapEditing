618c4e4a24d305d3cc5dd570d3ae2b19415f3588
Andreas Kurka <andreas.kurka@gmail.com>
2022-01-17 18:03:06 +0100

    Fixed binding issues Fixed DependencyProperties not working correctly because the type of the DependencyProperty owner was wrong

---
+++ MapCreationTool/MapCreationTool/Controls/LabeledTextBoxControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapCreationTool.Controls
+{
+    /// <summary>
+    /// Interaction logic for LabeledTextBoxControl.xaml
+    /// </summary>
+    public partial class LabeledTextBoxControl : UserControl
+    {
+		public string LabelText
+		{
+			get { return (string)GetValue(LabelTextProperty); }
+			set { SetValue(LabelTextProperty, value); }
+		}
+
+		public static readonly DependencyProperty LabelTextProperty = DependencyProperty.Register(
+			nameof(LabelText),
+			typeof(string),
+			typeof(LabeledTextBoxControl),
+			new PropertyMetadata("")
+		);
+
+		public string TextBoxValue
+		{
+			get { return (string)GetValue(TextBoxValueProperty); }
+			set { SetValue(TextBoxValueProperty, value); }
+		}
+
+		public static readonly DependencyProperty TextBoxValueProperty = DependencyProperty.Register(
+			nameof(TextBoxValue),
+			typeof(string),
+			typeof(LabeledTextBoxControl),
+			new PropertyMetadata("")
+		);
+
+		public LabeledTextBoxControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MapCreationTool/MapCreationTool/Controls/ProjectSettings.xaml.cs
+++ MapCreationTool/MapCreationTool/Controls/ProjectSettings.xaml.cs
@@ -29,7 +29,7 @@ namespace MapCreationTool.Controls
 		public static readonly DependencyProperty SettingHeightMapProperty = DependencyProperty.Register(
 			nameof(SettingHeightMap),
 			typeof(string),
-			typeof(CompileSetting),
+			typeof(ProjectSettings),
 			new PropertyMetadata("")
 		);
 
@@ -42,7 +42,7 @@ namespace MapCreationTool.Controls
 		public static readonly DependencyProperty SettingDiffuseMapProperty = DependencyProperty.Register(
 			nameof(SettingDiffuseMap),
 			typeof(string),
-			typeof(CompileSetting),
+			typeof(ProjectSettings),
 			new PropertyMetadata("")
 		);
 
