94b6fba82e21678abfd0227fb571f3eedf218d79
Andreas Kurka <andreas.kurka@adesso.de>
2022-01-20 13:12:09 +0100

    Added LuaEditor - not tested yet

---
+++ MapCreationTool/MapCreationTool/Lua/LuaEditor.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapCreationTool.Lua
+{
+	internal interface IValueProvider
+	{
+		object GetValue(string input);
+
+		string GetValueString(object input);
+	}
+
+	internal interface IValueProvider<T> : IValueProvider
+	{
+		T GetValue(string input);
+		string GetValueString(T input);
+	}
+
+	internal class IntProvider : IValueProvider<int?>
+	{
+		public object GetValue(string input)
+		{
+			return GetValue(input);
+		}
+		int? IValueProvider<int?>.GetValue(string input)
+		{
+			int result;
+			if (!int.TryParse(input, out result))
+				return null;
+
+			return result;
+		}
+
+		public string GetValueString(int? input)
+		{
+			return input?.ToString();
+		}
+
+		public string GetValueString(object input)
+		{
+			return GetValueString(input);
+		}
+
+	}
+
+	internal class DoubleProvider : IValueProvider<double?>, IValueProvider
+	{
+		public object GetValue(string input)
+		{
+			return GetValue(input);
+		}
+
+		double? IValueProvider<double?>.GetValue(string input)
+		{
+			double result;
+			if (!double.TryParse(input, out result))
+				return null;
+
+			return result;
+		}
+		public string GetValueString(double? input)
+		{
+			return input?.ToString(CultureInfo.InvariantCulture);
+		}
+
+		public string GetValueString(object input)
+		{
+			return GetValueString(input);
+		}
+
+
+	}
+
+
+	internal class StringProvider : IValueProvider<string>, IValueProvider
+	{
+		public object GetValue(string input)
+		{
+			return GetValue(input);
+		}
+
+		string IValueProvider<string>.GetValue(string input)
+		{
+			return input;
+		}
+
+		public string GetValueString(string input)
+		{
+			return $"\"input\"";
+		}
+
+		public string GetValueString(object input)
+		{
+			return GetValueString(input);
+		}
+	}
+
+	public class LuaEditor
+	{
+		string fileContent;
+		Dictionary<Type, IValueProvider> providers;
+
+		public LuaEditor()
+		{
+			providers = new Dictionary<Type, IValueProvider>();
+			providers.Add(typeof(int), new IntProvider());
+			providers.Add(typeof(string), new StringProvider());
+			providers.Add(typeof(double), new DoubleProvider());
+		}
+
+		public bool Load(string path)
+		{
+			try
+			{
+				using (StreamReader sr = new StreamReader(path))
+				{
+					fileContent = sr.ReadToEnd();
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine($"Error reading file: {path} - {ex.ToString()}");
+				return false;
+			}
+
+			return true;
+		}
+
+		public bool Save(string path)
+		{
+			try
+			{
+				using (StreamWriter sw = new StreamWriter(path))
+				{
+					sw.Write(fileContent);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine($"Error saving file: {path} - {ex.ToString()}");
+				return false;
+			}
+			return true;
+		}
+
+
+		public T GetValue<T>(string key) where T : struct
+		{
+			Type valueType = typeof(T);
+			if (!providers.ContainsKey(valueType))
+				throw new NotSupportedException($"Reading types of: {typeof(T).Name} is not supported");
+
+			string stringValue = GetValueFromContent(key);
+
+			T value = (T)providers[valueType].GetValue(stringValue);
+			return value;
+		}
+
+		public void SetValue<T>(string key, T value) where T : struct
+		{
+			Type valueType = typeof(T);
+			if (!providers.ContainsKey(valueType))
+				throw new NotSupportedException($"Reading types of: {typeof(T).Name} is not supported");
+
+			string valueString = providers[valueType].GetValueString(value);
+			SetValueInContent(key, valueString);
+		}
+
+
+		private string GetValueFromContent(string key)
+		{
+			int keyStartIdx = fileContent.IndexOf(key);
+			if (keyStartIdx == -1)
+				return null;
+
+			string subContent = fileContent.Substring(keyStartIdx);
+
+			int startValueIdx = subContent.IndexOf("=");
+			int endValueIdx = subContent.IndexOf("\n");
+
+			if (startValueIdx == -1 || endValueIdx == -1)
+				return null;
+
+			string rawContent = subContent.Substring(startValueIdx, endValueIdx - startValueIdx);
+			// Remove =, \n and whitespaces at beginning and end
+			string result = rawContent.Trim('=', '\n').TrimStart().TrimEnd();
+			return result;
+		}
+
+		private void SetValueInContent(string key, string value)
+		{
+			int keyStartIdx = fileContent.IndexOf(key);
+			if (keyStartIdx == -1)
+				return;
+
+			string subContent = fileContent.Substring(keyStartIdx);
+
+			int startValueIdx = subContent.IndexOf("=");
+			int endValueIdx = subContent.IndexOf("\n");
+
+			if (startValueIdx == -1 || endValueIdx == -1)
+				return;
+
+			string rawContent = subContent.Substring(startValueIdx, endValueIdx - startValueIdx);
+
+		}
+
+	}
+}
---
+++ MapCreationTool/MapCreationTool/TechnicalDocumentation.md
@@ -0,0 +1,55 @@
+﻿# Lua files
+It's pretty complicated to load and edit .lua files.
+
+To simplify parsing/editing a few decisions will be made.
+
+It also seems like there are no .lua libraries which help to resolve all issues
+
+# Relua
+* Ignores comments in file
+* Although the parser is on a very low level which is nice, it's too tedious to use
+
+# LsonLib
+* Does it allow to keep the sourcecode at the end of mapinfo.lua?
+* Does it support comments?
+
+# Custom parser/editor
+For now, we'll use the following design:
+The following approach should be easy to implement
+
+There's one major drawback however: If a key exists multiple times in the file, e.g. "name" appears in multiple structures
+it will only be possible to edit the first occurence. Fixing that by allowing indices will crash when one file doesn't have a definition, but another file has it.
+For that a proper context would be necessary. E.g. a path like: "startboxes.name", "player.name" etc, but this requires recursive parsing
+But for now, we can hopefully ignore this feature.
+
+Create an editor, which reads the entire file.
+
+public class LuaEditor
+{
+	public int GetIntValue(string key)
+	public string GetStringValue(string key)
+	public double GetDoubleValue(string key)
+	{
+		// searches for the given key, and returns everything between the = and , symbols
+	}
+
+	public void ReplaceIntValue(string key, int value)
+	public void ReplaceDoubleValue(string key, double value)
+	public void ReplaceStringValue(string key, string value)
+	{
+		// Searches for the given key, and replaces everything between the = and , symbols with the given number
+		// In case of string, quotes will be automatically added to the given value
+	}
+
+	public void Load(string path)
+	{
+		// Loads the given file into memory
+		// A simple: string contents = File.ReadAll should be sufficient
+	}
+
+	public void Save(string path)
+	{
+		// Writes the current data into the given path
+		// A simple: File.Write (contents) should be sufficient
+	}
+}
