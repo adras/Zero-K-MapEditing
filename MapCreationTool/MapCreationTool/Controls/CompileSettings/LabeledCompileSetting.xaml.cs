4eba9e32b0c9a67822e080e7ed6e472aae8e315e
Andreas Kurka <andreas.kurka@adesso.de>
2022-01-21 13:41:02 +0100

    Started restructuring of the UI Compile settings move to project settings compile dialog should only contain compile button and compilation result

---
+++ MapCreationTool/MapCreationTool/Controls/CompileSettings/LabeledCompileSetting.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapCreationTool.Controls.CompileSettings
+{
+    /// <summary>
+    /// Interaction logic for LabeledCompileSetting.xaml
+    /// </summary>
+    public partial class LabeledCompileSetting : UserControl
+    {
+        public bool SettingIsEnabled
+        {
+            get { return (bool)GetValue(SettingIsEnabledProperty); }
+            set { SetValue(SettingIsEnabledProperty, value); }
+        }
+
+        public bool SettingEnabledLocked
+        {
+            get { return (bool)GetValue(SettingEnabledLockedProperty); }
+            set { SetValue(SettingEnabledLockedProperty, value); }
+        }
+
+        public string SettingName
+        {
+            get { return (string)GetValue(SettingNameProperty); }
+            set { SetValue(SettingNameProperty, value); }
+        }
+
+        public string SettingDescription
+        {
+            get { return (string)GetValue(SettingDescriptionProperty); }
+            set { SetValue(SettingDescriptionProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty SettingIsEnabledProperty = DependencyProperty.Register(
+            nameof(SettingIsEnabled),
+            typeof(bool),
+            typeof(LabeledCompileSetting),
+            new PropertyMetadata(true)
+        );
+
+        public static readonly DependencyProperty SettingEnabledLockedProperty = DependencyProperty.Register(
+            nameof(SettingEnabledLocked),
+            typeof(bool),
+            typeof(LabeledCompileSetting),
+            new PropertyMetadata(true)
+        );
+
+        public static readonly DependencyProperty SettingNameProperty = DependencyProperty.Register(
+            nameof(SettingName),
+            typeof(string),
+            typeof(LabeledCompileSetting),
+            new PropertyMetadata("")
+        );
+
+        public static readonly DependencyProperty SettingDescriptionProperty = DependencyProperty.Register(
+            nameof(SettingDescription),
+            typeof(string),
+            typeof(LabeledCompileSetting),
+            new PropertyMetadata("")
+        );
+
+        public LabeledCompileSetting()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
---
+++ MapCreationTool/MapCreationTool/Controls/CompileSettings/ValueCompileSetting.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using Forms = System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapCreationTool.Controls.CompileSettings
+{
+	/// <summary>
+	/// Interaction logic for ValueCompileSetting.xaml
+	/// </summary>
+	public partial class ValueCompileSetting : UserControl
+	{
+		public bool SettingIsEnabled
+		{
+			get { return (bool)GetValue(SettingIsEnabledProperty); }
+			set { SetValue(SettingIsEnabledProperty, value); }
+		}
+
+		public bool SettingEnabledLocked
+		{
+			get { return (bool)GetValue(SettingEnabledLockedProperty); }
+			set { SetValue(SettingEnabledLockedProperty, value); }
+		}
+
+		public string SettingName
+		{
+			get { return (string)GetValue(SettingNameProperty); }
+			set { SetValue(SettingNameProperty, value); }
+		}
+
+		public string SettingValue
+		{
+			get { return (string)GetValue(SettingValueProperty); }
+			set { SetValue(SettingValueProperty, value); }
+		}
+
+		public string SettingDescription
+		{
+			get { return (string)GetValue(SettingDescriptionProperty); }
+			set { SetValue(SettingDescriptionProperty, value); }
+		}
+
+		public bool SettingHasFileBrowser
+		{
+			get { return (bool)GetValue(SettingHasFileBrowserProperty); }
+			set { SetValue(SettingHasFileBrowserProperty, value); }
+		}
+
+
+		public static readonly DependencyProperty SettingIsEnabledProperty = DependencyProperty.Register(
+			nameof(SettingIsEnabled),
+			typeof(bool),
+			typeof(ValueCompileSetting),
+			new PropertyMetadata(true)
+		);
+
+		public static readonly DependencyProperty SettingEnabledLockedProperty = DependencyProperty.Register(
+			nameof(SettingEnabledLocked),
+			typeof(bool),
+			typeof(ValueCompileSetting),
+			new PropertyMetadata(true)
+		);
+
+		public static readonly DependencyProperty SettingNameProperty = DependencyProperty.Register(
+			nameof(SettingName),
+			typeof(string),
+			typeof(ValueCompileSetting),
+			new PropertyMetadata("")
+		);
+
+		public static readonly DependencyProperty SettingValueProperty = DependencyProperty.Register(
+			nameof(SettingValue),
+			typeof(string),
+			typeof(ValueCompileSetting),
+			new PropertyMetadata("")
+		);
+
+		public static readonly DependencyProperty SettingDescriptionProperty = DependencyProperty.Register(
+			nameof(SettingDescription),
+			typeof(string),
+			typeof(ValueCompileSetting),
+			new PropertyMetadata("")
+		);
+
+		public static readonly DependencyProperty SettingHasFileBrowserProperty = DependencyProperty.Register(
+			nameof(SettingHasFileBrowser),
+			typeof(bool),
+			typeof(ValueCompileSetting),
+			new PropertyMetadata(false)
+		);
+
+		public ValueCompileSetting()
+		{
+			InitializeComponent();
+			DataContext = this;
+
+			// Default value
+			SettingEnabledLocked = false;
+		}
+
+		private void btnBrowseFile_Click(object sender, RoutedEventArgs e)
+		{
+			Forms.SaveFileDialog dialog = new Forms.SaveFileDialog();
+			dialog.Filter = "smt files (*.smt)|*.smt|All files (*.*)|*.*";
+			dialog.CheckFileExists = false;
+			dialog.AddExtension = true;
+			Forms.DialogResult result = dialog.ShowDialog();
+
+			if (result != Forms.DialogResult.OK)
+				return;
+
+			SettingValue = dialog.FileName;
+		}
+	}
+}
