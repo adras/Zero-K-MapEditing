ab58b38e05e1c9381e190d3b9d810828be6b591c
Andreas Kurka <andreas.kurka@gmail.com>
2022-02-05 14:30:04 +0100

    Removed some vars

--- MapCreationTool/MapCreationTool/NewRendering/Shader.cs
+++ MapCreationTool/MapCreationTool/NewRendering/Shader.cs
@@ -25,10 +25,10 @@ namespace MapCreationTool.NewRendering
             //   The fragment shader is what we'll be using the most here.
 
             // Load vertex shader and compile
-            var shaderSource = File.ReadAllText(vertPath);
+            string? shaderSource = File.ReadAllText(vertPath);
 
             // GL.CreateShader will create an empty shader (obviously). The ShaderType enum denotes which type of shader will be created.
-            var vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
 
             // Now, bind the GLSL source code
             GL.ShaderSource(vertexShader, shaderSource);
@@ -38,7 +38,7 @@ namespace MapCreationTool.NewRendering
 
             // We do the same for the fragment shader.
             shaderSource = File.ReadAllText(fragPath);
-            var fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
             GL.ShaderSource(fragmentShader, shaderSource);
             CompileShader(fragmentShader);
 
@@ -65,19 +65,19 @@ namespace MapCreationTool.NewRendering
             // later.
 
             // First, we have to get the number of active uniforms in the shader.
-            GL.GetProgram(Handle, GetProgramParameterName.ActiveUniforms, out var numberOfUniforms);
+            GL.GetProgram(Handle, GetProgramParameterName.ActiveUniforms, out int numberOfUniforms);
 
             // Next, allocate the dictionary to hold the locations.
             _uniformLocations = new Dictionary<string, int>();
 
             // Loop over all the uniforms,
-            for (var i = 0; i < numberOfUniforms; i++)
+            for (int i = 0; i < numberOfUniforms; i++)
             {
                 // get the name of this uniform,
-                var key = GL.GetActiveUniform(Handle, i, out _, out _);
+                string? key = GL.GetActiveUniform(Handle, i, out _, out _);
 
                 // get the location,
-                var location = GL.GetUniformLocation(Handle, key);
+                int location = GL.GetUniformLocation(Handle, key);
 
                 // and then add it to the dictionary.
                 _uniformLocations.Add(key, location);
@@ -98,11 +98,11 @@ namespace MapCreationTool.NewRendering
             GL.CompileShader(shader);
 
             // Check for compilation errors
-            GL.GetShader(shader, ShaderParameter.CompileStatus, out var code);
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out int code);
             if (code != (int)All.True)
             {
                 // We can use `GL.GetShaderInfoLog(shader)` to get information about the error.
-                var infoLog = GL.GetShaderInfoLog(shader);
+                string? infoLog = GL.GetShaderInfoLog(shader);
                 throw new Exception($"Error occurred whilst compiling Shader({shader}).\n\n{infoLog}");
             }
         }
@@ -113,7 +113,7 @@ namespace MapCreationTool.NewRendering
             GL.LinkProgram(program);
 
             // Check for linking errors
-            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out var code);
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out int code);
             if (code != (int)All.True)
             {
                 // We can use `GL.GetProgramInfoLog(program)` to get information about the error.
